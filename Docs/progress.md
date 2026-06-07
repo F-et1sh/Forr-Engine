894580028de45b10770fb6ad3deff6107f8d2eb5
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2026-06-07 13:21:09 +0300

    VMA added

--- Docs/progress.md
+++ Docs/progress.md
@@ -1,5 +1,21 @@
 ﻿# Forr-Engine Devlog
 
+## 07.06.2026
+### Goal
+    provide tools to decrease Vulkan boilerplate code
+    provide texture's concept to Vulkan ( don't push to the shader, CPU only )
+    chnage GLSL to Slang
+    provide debug tools with Dear ImGui
+    ( fix window resizing for GL/VK )
+    ( do not create GPU resources if they're already created --> InitializeGPUResources() )
+
+### Done
+    VMA submodule added
+    VMA added
+
+### Problem
+    -
+
 ## 04.06.2026
 ### Goal
     provide tools to decrease Vulkan boilerplate code
---
+++ Scripts/VulkanMemoryAllocator/copy_to_third_party.bat
@@ -0,0 +1,27 @@
+::===============================================
+::
+::  Forr Engine - Scripts
+::
+::  File : copy_to_third_party.bat
+::  Role : copy Vulkan Memory Allocator include and license
+::		to its folder in ThirdParty
+::
+::  Copyright (C) 2026 Farrakh
+::  All Rights Reserved.
+::
+::===============================================
+
+@echo off
+
+xcopy /y /i /s "..\\..\\External\\VulkanMemoryAllocator\\include\\vk_mem_alloc.h" "..\\..\\ThirdParty\\VulkanMemoryAllocator\\include\\" >nul
+copy "..\\..\\External\\VulkanMemoryAllocator\\LICENSE.txt" "..\\..\\ThirdParty\\VulkanMemoryAllocator\\VMA_LICENSE.txt" >nul
+
+mkdir "..\\..\\ThirdParty\\VulkanMemoryAllocator\\src"
+
+(
+echo #define VMA_IMPLEMENTATION
+echo #define VMA_STATIC_VULKAN_FUNCTIONS 0
+echo #define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
+echo #include "Volk/volk.h"
+echo #include "../include/vk_mem_alloc.h"
+) > "..\\..\\ThirdParty\\VulkanMemoryAllocator\\src\\vk_mem_alloc.cpp"
